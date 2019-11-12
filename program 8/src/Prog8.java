8402ef256c3b8074e0d03f3c3ab13a54d68d4a0a
tderocco <tderocco@10.242.5.102>
2019-11-12 17:33:13 -0500

    ready

---
+++ program 8/src/Prog8.java
@@ -0,0 +1,57 @@
+// Author	Talon DeRocco
+// Date		11.12.19
+// Project	Program 8
+
+// file usage
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Prog8 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		FileWriter fw = new FileWriter("/Users/talonderocco/Desktop/gradesOut.txt", true);
+		PrintWriter outputFile = new PrintWriter(fw);
+		
+		// Create a File
+		File myFile = new File("/Users/talonderocco/Desktop/grades.txt");
+		Scanner inputFile = new Scanner(myFile);
+		
+		// variables
+		double totalPoints = 0.00;
+		double totalGrades = 0.00;
+		double totalAverage = 0.00;
+		
+		while (inputFile.hasNext())
+		{
+			char category = inputFile.next().charAt(0);
+			double catAmount = inputFile.nextDouble();
+			
+			switch(category)
+			{
+			case 'A':
+				totalPoints+=catAmount;
+				break;
+			case 'B':
+				totalGrades+=catAmount;
+				break;
+			case 'C':
+				totalAverage+=catAmount;
+				break;
+			}
+		}
+		
+		outputFile.println("Total Number of Points Scored");
+		outputFile.println("===================");
+		outputFile.printf("Category A:\t$%.2f\n", totalPoints);
+		outputFile.printf("Category B:\t$%.2f\n", totalGrades);
+		outputFile.printf("Category C:\t$%.2f\n", totalAverage);
+		
+		// Close files
+		outputFile.close();
+		InputFile.close();
+	}
+
+}

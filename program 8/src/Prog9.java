60cbccba00029975f021e49a9edeca523ee7020a
tderocco <tderocco@10.0.0.164>
2019-11-19 23:32:37 -0500

    ready

--- program 8/src/Prog9.java
+++ program 8/src/Prog9.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.Scanner;
 
 
-public class Prog8 {
+public class Prog9 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
